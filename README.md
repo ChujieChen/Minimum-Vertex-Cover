978559ff25a886cadd3db3a493543eaf83631c8d
Chujie Chen <cchen641@gatech.edu>
2020-12-05 15:29:48 -0800

    include executable

--- README.md
+++ README.md
@@ -1,23 +1,51 @@
 # Minimum Vertex Cover
-Group 4 competition-only branch. The program has been compiled on a mac machine. `.class` files are included in this branch.
+Group 4 competition-only branch. The program has been compiled on a mac machine. The `group4.jar` file is included in this branch as the executable.
 
-## Compile (if needed)
-At the project root directory run:
+## Usage Example
 ```
-javac $(find . -name "*.java")
+java -jar group4.jar -inst power.graph -alg LS -time 60 -seed 9
+```
+
+You can move the `.jar` file to any directory but please make sure graphs are in the same folder with the `.jar`.
+
+If the `group4.jar` does not work, please see below to compile and run the algorithm yourself.
+
+## Track(s)
+LS
+
+## Approach Description
+
+### BnB
+N/A.
+
+### LS
+Hill Combling: starting from a random initial VC, remove vertices in ascending order based on vertex degrees.
+
+Time complexity: O(|E| + |V| log |V|)
+
+# If group4.jar Didn't Work
+## Compile (if needed)
+At the project present directory `/path/to/project` run:
+```
+javac $(find . -name "*.java") -d bin
 ```
 
 ## Run MVC
-Change directory to `/src`, then run 
+Change directory to `/bin`
 
-`java main.java.JavaAlgo -inst <filename> -alg [BnB|LS] -time <cutoff in seconds> [-seed <random seed>]`.
+`cd bin`
+
+then run
+
+`java main.java.JavaAlgo -inst <filename> -alg [LS] -time <cutoff in seconds> [-seed <random seed>]`.
 
 An example shows below:
 
 ```bash
-cd src    # now at /path/to/project/src/
-java main.java.JavaAlgo -inst jazz.graph -alg LS -time 600 -seed 9
+cd bin    # now at /path/to/project/bin/
+java main.java.JavaAlgo -inst power.graph -alg LS -time 60 -seed 100
 ```
 
-## Data and Output
-All graphs must be under `src/data` folder. Outputs will exist under `src/output` folder.
+## Other
+
+See [here](https://github.com/ChujieChen/Minimum-Vertex-Cover/tree/develop) to get the latest readme.
--- src/main/java/JavaAlgo.java
+++ src/main/java/JavaAlgo.java
@@ -1,14 +1,14 @@
 package main.java;
 
-import java.io.File;
 
+import java.io.File;
 import main.java.method.BnB.BranchAndBound;
 import main.java.method.LS.HillClimbing;
 
 public class JavaAlgo {
     public static void main(String[] args) {
     	System.out.println("running on..." + System.getProperty("user.dir"));
-        File file=new File("./output");
+        File file = new File("./output");
         if(!file.exists()){//if the dir not exist
             file.mkdir();//create the dir
         }
@@ -19,7 +19,6 @@ public class JavaAlgo {
             int seed = 0;
             
         	for (int i = 0; i < args.length; i+=2) {
-
         		switch (args[i]) {
 				case "-inst":
 					filename = args[i + 1];
@@ -42,16 +41,16 @@ public class JavaAlgo {
             Algo algo = null;
             
             String graphPrefix = filename.substring(0, filename.length() - 6);
-            String inFileName = "./data/" + filename;
+            String inFileName = "" + filename;
             String outSol = "./output/";
             String outTrace = "./output/";
             
             switch (algoName){
-                case "BnB":
-                    algo = new BranchAndBound();
-                    outSol = outSol+graphPrefix+"_BnB_"+time+".sol";
-                    outTrace = outTrace+graphPrefix+"_BnB_"+time+".trace";
-                    break;
+//                case "BnB":
+//                    algo = new BranchAndBound();
+//                    outSol = outSol+graphPrefix+"_BnB_"+time+".sol";
+//                    outTrace = outTrace+graphPrefix+"_BnB_"+time+".trace";
+//                    break;
                 case "LS":
                     algo = new HillClimbing();
                     outSol = outSol+graphPrefix+"_LS_"+time+"_"+seed+".sol";
@@ -61,7 +60,6 @@ public class JavaAlgo {
                     System.out.println("ERROR: unimplemented algorithm");
             }
             algo.run(inFileName, outSol, outTrace, time, seed);
-
         }catch (Exception e){
             System.out.println("Wrong arguments: " + e.toString());
         }
